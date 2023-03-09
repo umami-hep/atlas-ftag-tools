363184e18eefab20fd16bf3edc089b9bf8a80d96
samvanstroud <sam.van.stroud@cern.ch>
2023-03-09 14:01:27 +0000

    add region

---
+++ ftag/src/region.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from jetpp.classes.cuts import Cuts
+
+
+@dataclass(frozen=True)
+class Region:
+    name: str
+    cuts: Cuts
+
+    def __str__(self):
+        return self.name
+
+    def __lt__(self, other):
+        return self.cuts[0].value < other.cuts[0].value
+
+    def __eq__(self, other):
+        return self.name == other.name
