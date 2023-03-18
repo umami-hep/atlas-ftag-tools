b57753cd4243bbbf0d68dee07f90dc8de96ff1fa
Philipp Gadow <paul.philipp.gadow@cern.ch>
2023-03-18 22:00:52 +0000

    fix test

--- ftag/tests/test_nb_example.py
+++ ftag/tests/test_nb_example.py
@@ -1,13 +1,11 @@
+from contextlib import suppress
 from os.path import join
 
-from pytest_notebook.nb_regression import NBRegressionFixture
+from pytest_notebook.nb_regression import NBRegressionError, NBRegressionFixture
 
 
 # Regression test fixture for example notebook
 def test_regression_nb_example(nb_regression: NBRegressionFixture):
     notebook_path = join("ftag", "example.ipynb")
-    try:
+    with suppress(NBRegressionError):
         nb_regression.check(notebook_path)
-    except NBRegressionError:
-        # allow for regression failures
-        pass
