6406362f007a78dd55016a38ff3a2d3fc2906057
Sam Van Stroud <sam.van.stroud@cern.ch>
2023-04-04 14:46:21 +0100

    use short git hash

--- ftag/hdf5/h5writer.py
+++ ftag/hdf5/h5writer.py
@@ -30,7 +30,8 @@ class H5Writer:
         self.dst.parent.mkdir(parents=True, exist_ok=True)
         self.file = h5py.File(self.dst, "w")
         self.add_attr("srcfile", str(self.src))
-        self.git_hash = check_output(["git", "rev-parse", "HEAD"]).decode("ascii").strip()
+        git_hash = check_output(["git", "rev-parse", "--short", "HEAD"])
+        self.git_hash = git_hash.decode("ascii").strip()
         self.add_attr("git_hash", self.git_hash)
         for name, var in self.variables.items():
             self.create_ds(name, var)
