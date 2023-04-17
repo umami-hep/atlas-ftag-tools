266ce9070d4db1507a26f0dcc0ec6be8dc81b4e2
Sam VS <sam.van.stroud@cern.ch>
2023-04-17 12:56:24 +0100

    Update WP script with configurable cuts (#18)
    * tets
    
    * add docstring, fix bug
    
    * update changelog

--- ftag/wps/discriminant.py
+++ ftag/wps/discriminant.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from ftag.flavour import Flavours
+from ftag.flavour import Flavour, Flavours
 
 
 def btag_discriminant(pb, pc, pu, fc=0.1, epsilon=1e-10):
@@ -11,7 +11,33 @@ def ctag_discriminant(pb, pc, pu, fb=0.2, epsilon=1e-10):
     return np.log((pc + epsilon) / ((1.0 - fb) * pu + fb * pb + epsilon))
 
 
-def get_discriminant(jets, tagger, signal, fx=0.1, epsilon=1e-10):
-    disc_func = btag_discriminant if Flavours[signal] == Flavours.bjets else ctag_discriminant
+def get_discriminant(
+    jets: np.ndarray, tagger: str, signal: Flavour, fx: float, epsilon: float = 1e-10
+):
+    """Calculate the b-tag or c-tag discriminant for a given tagger.
+
+    Parameters
+    ----------
+    jets : np.ndarray
+        Structured array of jets containing tagger outputs
+    tagger : str
+        Name of the tagger
+    signal : Flavour
+        Signal flavour (bjets or cjets)
+    fx : float, optional
+        Value fb or fc
+    epsilon : float, optional
+        Small number to avoid division by zero, by default 1e-10
+
+    Returns
+    -------
+    np.ndarray
+        Array of discriminant values.
+    """
     pb, pc, pu = (jets[f"{tagger}_pb"], jets[f"{tagger}_pc"], jets[f"{tagger}_pu"])
-    return disc_func(pb, pc, pu, fx, epsilon)
+    if Flavours[signal] == Flavours.bjets:
+        return btag_discriminant(pb, pc, pu, fx, epsilon)
+    elif Flavours[signal] == Flavours.cjets:
+        return ctag_discriminant(pb, pc, pu, fx, epsilon)
+    else:
+        raise ValueError(f"Signal flavour must be bjets or cjets, not {signal}")
