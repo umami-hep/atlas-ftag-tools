be964245a45147885547c56fc26a0625939254d0
Sam Van Stroud <sam.van.stroud@cern.ch>
2023-04-17 14:46:30 +0100

    prepare for release

--- ftag/__init__.py
+++ ftag/__init__.py
@@ -1,12 +1,12 @@
 """atlas-ftag-tools - Common tools for ATLAS flavour tagging software."""
 
 
-__version__ = "v0.0.8"
+__version__ = "v0.0.9"
 
 
 import ftag.hdf5 as hdf5
 from ftag.cuts import Cuts
-from ftag.flavour import Flavours
+from ftag.flavour import Flavour, Flavours
 from ftag.mock import get_mock_file
 from ftag.sample import Sample
 from ftag.wps.discriminant import get_discriminant
@@ -14,6 +14,7 @@ from ftag.wps.working_points import get_working_points
 
 __all__ = [
     "Cuts",
+    "Flavour",
     "Flavours",
     "Sample",
     "hdf5",
