f064613666852d358ece6a5faf52f084b8aabac3
jburzy <jackson.carl.burzynski@cern.ch>
2024-01-16 08:25:05 -0800

    add option to set custom yaml path

--- ftag/flavour.py
+++ ftag/flavour.py
@@ -43,6 +43,17 @@ class Flavour:
 class FlavourContainer:
     flavours: dict[str, Flavour]
 
+    def __init__(self, yaml_path: Path = None):
+        if yaml_path is None:
+            yaml_path = Path(__file__).parent / "flavours.yaml"
+        with open(yaml_path) as f:
+            flavours_yaml = yaml.safe_load(f)
+        
+        flavours_dict = {f["name"]: Flavour(cuts=Cuts.from_list(f.pop("cuts")), **f) for f in flavours_yaml}
+        assert len(flavours_dict) == len(flavours_yaml), "Duplicate flavour names detected"
+
+        self.flavours = flavours_dict
+
     def __iter__(self) -> Generator:
         yield from self.flavours.values()
 
@@ -80,9 +91,4 @@ class FlavourContainer:
                 return flavour
         raise KeyError(f"Flavour with {cuts} not found")
 
-
-with open(Path(__file__).parent / "flavours.yaml") as f:
-    flavours_yaml = yaml.safe_load(f)
-flavours_dict = {f["name"]: Flavour(cuts=Cuts.from_list(f.pop("cuts")), **f) for f in flavours_yaml}
-assert len(flavours_dict) == len(flavours_yaml), "Duplicate flavour names detected"
-Flavours = FlavourContainer(flavours_dict)
+Flavours = FlavourContainer()
