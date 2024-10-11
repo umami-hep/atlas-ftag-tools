89f0eeb27a11084f7f677bbd881d8cf65b9c2c70
Wei Sheng Lai <wlai@hypatia-login.local>
2024-10-11 09:25:26 +0100

    added temp ghost disc

--- ftag/wps/discriminant.py
+++ ftag/wps/discriminant.py
@@ -63,6 +63,11 @@ def btag_discriminant(jets, tagger, fc, ftau=0, epsilon=1e-10):
     return discriminant(jets, tagger, Flavours.bjets, fxs, epsilon=epsilon)
 
 
+def ghostbtag_discriminant(jets, tagger, fc, ftau=0, epsilon=1e-10):
+    fxs = {"pghostc": fc, "pghosttau": ftau, "pghostu": 1 - fc - ftau}
+    return discriminant(jets, tagger, Flavours.ghostbjets, fxs, epsilon=epsilon)
+
+
 def ctag_discriminant(jets, tagger, fb, ftau=0, epsilon=1e-10):
     fxs = {"pb": fb, "ptau": ftau, "pu": 1 - fb - ftau}
     return discriminant(jets, tagger, Flavours.cjets, fxs, epsilon=epsilon)
@@ -112,6 +117,7 @@ def get_discriminant(
         "taujets": tautag_dicriminant,
         "hbb": hbb_discriminant,
         "hcc": hcc_discriminant,
+        "ghostbjets": ghostbtag_discriminant,
     }
 
     if str(signal) not in tagger_funcs:
