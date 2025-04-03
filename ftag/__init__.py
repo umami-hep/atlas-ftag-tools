d7ada6559153afc82a97b302a08e1ef81f7f6df7
Alexander Froch <Alexander.Froch@cern.ch>
2025-04-03 11:10:05 +0200

    Moving big parts of the puma metrics into atlas-ftag-tools

--- ftag/__init__.py
+++ ftag/__init__.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 __version__ = "v0.2.9"
 
-from ftag import hdf5
+from ftag import hdf5, utils
 from ftag.cuts import Cuts
 from ftag.flavours import Flavours
 from ftag.labeller import Labeller
@@ -12,8 +12,7 @@ from ftag.labels import Label, LabelContainer
 from ftag.mock import get_mock_file
 from ftag.sample import Sample
 from ftag.transform import Transform
-from ftag.wps.discriminant import get_discriminant
-from ftag.wps.working_points import get_working_points
+from ftag.working_points import get_working_points
 
 __all__ = [
     "Cuts",
@@ -24,8 +23,8 @@ __all__ = [
     "Sample",
     "Transform",
     "__version__",
-    "get_discriminant",
     "get_mock_file",
     "get_working_points",
     "hdf5",
+    "utils",
 ]
