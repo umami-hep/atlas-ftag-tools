9a2c559236c4325a4b71968399d4fb220a205517
Alexander Froch <Alexander.Froch@cern.ch>
2025-08-06 12:16:51 +0200

    Updating flavours and allow duplicated flavours in different categories

--- ftag/flavours.py
+++ ftag/flavours.py
@@ -2,4 +2,15 @@ from __future__ import annotations
 
 from ftag.labels import LabelContainer
 
-Flavours = LabelContainer.from_yaml()
+Flavours = LabelContainer.from_yaml(
+    exclude_categories=[
+        "single-btag-extended",
+        "single-btag-extended-ghost",
+    ]
+)
+Extended_Flavours = LabelContainer.from_yaml(
+    exclude_categories=[
+        "single-btag",
+        "single-btag-ghost",
+    ]
+)
